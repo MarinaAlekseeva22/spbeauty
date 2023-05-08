5bb62f5ee24db72a4634ba63473dc8159b3e6ee1
MarinaAlekseeva22 <130898632+MarinaAlekseeva22@users.noreply.github.com>
2023-05-08 05:53:30 +0000

    поменяла праймери баттон

--- src/Components/PrimaryButton.js
+++ src/Components/PrimaryButton.js
@@ -4,12 +4,12 @@ import styled from "styled-components";
 
 
 function PrimaryButton(props) {
-    // Здесь устанавливается минимальная ширина для кнопки
+    <PrimaryButton>Нормалбная кнобка</PrimaryButton>
     let minWidth = props.minWidth === undefined ? '144px' : props.minWidth; 
     const Button = styled.div`
-        background: #4B759E;
+        background: #3B82F6;
         border: 1px solid #152842;
-        box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
+        border-radius: 24px;
         min-width: ${minWidth};
         color: white;
         &:hover {
